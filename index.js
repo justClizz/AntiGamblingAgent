2e9ba833dcb2ea2660c763e29e20e3cfa48982ac
Muhammad Haikal <75706383+justClizz@users.noreply.github.com>
2022-04-14 18:51:03 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const execSync = require('child_process').execSync;
+const P = require('pino')
+const { 
+default: makeWASocket, 
+DisconnectReason, 
+AnyMessageContent, 
+delay,
+proto,
+jidDecode,
+useSingleFileAuthState,
+generateForwardMessageContent, 
+generateWAMessageFromContent,
+downloadContentFromMessage, 
+generateMessageID,
+makeInMemoryStore
+} = require('@adiwajshing/baileys')
+const baileys = require('@adiwajshing/baileys')
+const fs = require("fs")
+const fsx = require('fs-extra')
+const axios = require('axios')
+const { Boom } = require("@hapi/boom")
+const { state, loadState, saveState } = useSingleFileAuthState("./chan.json")
+const store = makeInMemoryStore({ logger: P().child({ level: 'silent', stream: 'store' }) })
+
+
+const getVersionWaweb = () => {
+    let version
+    try {
+        let { data } = axios.get('https://web.whatsapp.com/check-update?version=1&platform=web')
+        version = [data.currentVersion.replace(/[.]/g, ', ')]
+    } catch {
+        version = [2, 2204, 13]
+    }
+    return version
+}
+
+const startSock = () => {
+const chan = makeWASocket({
+    logger: P({ level: 'fatal' }),
+    printQRInTerminal: true,
+    auth: state,
+    browser: ["AntiGamblingAgent By justClizz","Safari","1.0.0"],
+    version: getVersionWaweb() || [2, 2204, 13]
+})
+
+chan.ev.on('messages.upsert', async m => {
+const msg = m.messages[0]
+const from = msg.key.remoteJid
+const content = JSON.stringify(msg.message)
+
+const type = Object.keys(msg.message)[0]
+const text = (type === 'conversation' && msg.message.conversation) ? msg.message.conversation: (type == 'imageMessage') && msg.message.imageMessage.caption ? (msg.message.imageMessage.caption || ''): (type == 'videoMessage') && msg.message.videoMessage.caption ? msg.message.videoMessage.caption: (type == 'extendedTextMessage') && msg.message.extendedTextMessage.text ? msg.message.extendedTextMessage.text: ''
+const isiChat = text
+
+if (!msg.key.fromMe && m.type === 'notify') {
+
+if (isiChat.includes("bet") || isiChat.includes("slot") || isiChat.includes("Slot") || isiChat.includes("judi") || isiChat.includes("Judi")) {
+ chan.sendMessage(msg.key.remoteJid, { text: `Maaf, kita tidak mau berjudi dan GAK AKAN PERNAH JUDI. Terimakasih atas tawarannya.`})
+ await delay(800)
+ await chan.updateBlockStatus(msg.key.remoteJid, "block")
+}             
+}
+})
+
+chan.ev.on('creds.update', saveState)
+}
+
+startSock()
